e1ae14359d142e85c11d14920e8ed4586cdaadd4
Dijostwn <139295813+Dijostwn@users.noreply.github.com>
2025-12-10 23:56:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -36,12 +36,6 @@ mongoose.connect(MONGODB_URI)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(PUBLIC_DIR)); 
 
-// Pastikan root path ('/') selalu melayani index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-// ----------------------
-
 // ENDPOINT KIRIM TIKET
 app.post('/kirim-tiket', async (req, res) => { // Fungsi harus ASYNC
     const dataTiket = req.body;
